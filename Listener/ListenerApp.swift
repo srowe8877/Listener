39e884697b0fb70b47764be739e753212e2ae5eb
srowe8877 <srowe8877@gmail.com>
2024-12-10 04:58:47 -0600

    Initial Commit

---
+++ Listener/ListenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenerApp.swift
+//  Listener
+//
+//  Created by Stephen Rowe on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ListenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
